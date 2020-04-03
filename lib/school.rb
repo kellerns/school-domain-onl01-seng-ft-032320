e6ba2b43bbef5276994559bcae2b6c2752da256b
Nicholas Keller <nic.keller328@gmail.com>
2020-04-03 15:26:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  attr_reader   :name, :roster, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+    
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @grade = grade
+    @roster.each do |grade, names_array|
+      if grade == @grade
+        @roster[grade] << name
+      end
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+    
+  def sort
+    roster = @roster
+    roster.collect do |grade, names|
+      names.sort
+    end
+    roster
+  end
+  
+  
+end
